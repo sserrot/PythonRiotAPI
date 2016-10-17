c43c82d7a2d2d17e337a790b86dcf6994805b99d
Santiago Torres <santiago6695@gmail.com>
2016-10-17 11:51:05 -0400

    display results in html and remove API key

--- server.py
+++ server.py
@@ -8,30 +8,27 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+api_key = str(input())
 riotapi.set_region("NA")
-riotapi.set_api_key("2af3426a-6e8a-4138-8eab-d88c430852c1")
-globalSummoner = ''
+riotapi.set_api_key(api_key)
+globalSummoner = ' '
 
 # Index Initialized
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def index():
-    if request.method == 'POST':
-        summoner_name = request.form['summoner_name']
-        summoner = riotapi.get_summoner_by_name(summoner_name)
-        print("{name} is a level {level} summoner on the NA server.".format(name=summoner.name, level=summoner.level))
     return render_template('index.html')
 
 
-# @app.route('/display')
-# def summoner_test():
-#     if request.method == 'POST':
-#         summoner_name = request.form['summoner_name']
-#         summoner = riotapi.get_summoner_by_name(summoner_name)
-#         print("{name} is a level {level} summoner on the NA server.".format(name=summoner.name, level=summoner.level))
-#     return "{name} is a level {level} summoner on the NA server.".format(name=summoner.name, level=summoner.level)
+@app.route("/display.html", methods=['GET', 'POST'])
+def display():
+    if request.method == 'POST':
+        summoner_name = request.form['summoner_name']
+        summoner = riotapi.get_summoner_by_name(summoner_name)
+        globalSummoner = "{name} is a level {level} summoner on the NA server.".format(name=summoner.name, level=summoner.level)
+        return render_template('display.html', result=globalSummoner)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run()
